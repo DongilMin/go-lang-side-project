202858772d188182e9f8f1c8d68a18aa478a6160
DongilMin <tasabi0826@gmail.com>
2025-07-17 20:48:12 +0900

    feat: set env and db

---
+++ database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+    "gorm.io/driver/sqlite"
+    "gorm.io/gorm"
+    "log"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+    var err error
+    DB, err = gorm.Open(sqlite.Open("shopping_mall.db"), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
+    
+    log.Println("Database connected successfully")
+}
