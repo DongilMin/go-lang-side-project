b7d75fd380f9025610bf342c046a2147fc977676
DongilMin <tasabi0826@gmail.com>
2025-07-18 19:55:59 +0900

    Refactor : HARD_CODING at DB Connection KEY

--- database/database.go
+++ database/database.go
@@ -1,19 +1,26 @@
 package database
 
 import (
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
-    "log"
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-    var err error
-    DB, err = gorm.Open(sqlite.Open("shopping_mall.db"), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-    }
-    
-    log.Println("Database connected successfully")
+	var err error
+	
+	// 하드코딩된 SQLite 연결 설정
+	DB, err = gorm.Open(sqlite.Open("shopping_mall.db"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	
+	log.Println("Database connected successfully")
 }
