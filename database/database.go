5f9139d5d4fe67f2dd941164faebd4f5cec31f6a
DongilMin <tasabi0826@gmail.com>
2025-07-18 19:56:53 +0900

    feat : add new column at product model

--- database/database.go
+++ database/database.go
@@ -13,7 +13,6 @@ var DB *gorm.DB
 func Connect() {
 	var err error
 	
-	// 하드코딩된 SQLite 연결 설정
 	DB, err = gorm.Open(sqlite.Open("shopping_mall.db"), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
--- models/product.go
+++ models/product.go
@@ -1,17 +1,18 @@
 package models
 
 import (
-    "time"
-    "gorm.io/gorm"
+	"time"
+
+	"gorm.io/gorm"
 )
 
 type Product struct {
 	ID          uint           `json:"id" gorm:"primaryKey"`
-	Name        string         `json:"name" gorm:"not null"`
-	Description string         `json:"description"`
-	Price       float64        `json:"price" gorm:"not null"`
-	Stock       int            `json:"stock" gorm:"default:0"`
-	CategoryID  uint           `json:"category_id"`
+	Name        string         `json:"name" gorm:"not null;size:255"`
+	Description string         `json:"description" gorm:"type:text"`
+	Price       float64        `json:"price" gorm:"not null;check:price >= 0"`
+	Stock       int            `json:"stock" gorm:"default:0;check:stock >= 0"`
+	CategoryID  uint           `json:"category_id" gorm:"not null"`
 	Category    Category       `json:"category,omitempty" gorm:"foreignKey:CategoryID"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
