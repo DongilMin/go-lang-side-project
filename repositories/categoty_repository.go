d719da972fead38c4cdc9f650eb382f98d5eeee5
DongilMin <tasabi0826@gmail.com>
2025-07-18 20:03:23 +0900

    feat : add new logic in category repo

--- repositories/categoty_repository.go
+++ repositories/categoty_repository.go
@@ -20,3 +20,30 @@ type categoryRepository struct {
 func NewCategoryRepository(db *gorm.DB) CategoryRepository {
 	return &categoryRepository{db: db}
 }
+
+func (r *categoryRepository) GetAll() ([]models.Category, error) {
+	var categories []models.Category
+	err := r.db.Find(&categories).Error
+	return categories, err
+}
+
+func (r *categoryRepository) GetByID(id uint) (*models.Category, error) {
+	var category models.Category
+	err := r.db.Preload("Products").First(&category, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (r *categoryRepository) Create(category *models.Category) error {
+	return r.db.Create(category).Error
+}
+
+func (r *categoryRepository) Update(category *models.Category) error {
+	return r.db.Save(category).Error
+}
+
+func (r *categoryRepository) Delete(id uint) error {
+	return r.db.Delete(&models.Category{}, id).Error
+}
